1507b1ce698dc42a861cc611f0027c0597aed784
Dimickde <dj@dimickdesignllc.com>
2017-07-03 12:04:59 -0700

    Updated

--- Calculator.php
+++ Calculator.php
@@ -8,7 +8,11 @@ include_once('inc/header.php');
 	<label for="IntOne">Input1: </label>
 	<input type = "number" name = "IntOne" class="form-control" placeholder="Input1...">
 	</div>
-	Input2: <input type = "number" name = "IntTwo"> <br>
+	<div class="form_group">
+	<label for="IntTwo">Input1: </label>
+	<input type = "number" name = "IntTwo" class="form-control" placeholder="Input2...">
+	</div>
+	
 	<input type = "submit" name = "add" value = "Add"> 
 	<input type = "submit" name = "subtract" value = "Subtract">
 </form>
